310cd62bd7f29279165eae691eadf8594d880cae
William Gaines <willny24@gmail.com>
2022-03-25 17:46:59 -0400

    added log4j properties and gradle dependencies
    created a log4j2.properties file for use when logging errors throughout the API,
    added dependencies for log4j, postgresql, and bcrypt.

---
+++ src/main/java/com/revature/Application.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+public class Application
+{
+    //run is used to initialize the application.
+    public static void run()
+    {
+        loggerInit();
+    }
+
+    private static void loggerInit()
+    {
+
+    }
+}
--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,10 +1,16 @@
 package com.revature;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Driver
 {
+    private static final Logger logger = LogManager.getLogger(Driver.class);
+
+    //Entry point of the application.
     public static void main(String args[])
     {
-
+        logger.info("Starting Application");
+        Application.run();
     }
 }
