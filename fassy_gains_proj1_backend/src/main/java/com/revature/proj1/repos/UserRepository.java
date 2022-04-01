508df4dc392cd894da7ad46aeafbbe47782ad70c
Christian Fassy <christianfassy@gmail.com>
2022-03-31 23:17:29 -0400

    Added web.xml and some changes to build.gradle and other user files

--- fassy_gains_proj1_backend/src/main/java/com/revature/proj1/repos/UserRepository.java
+++ fassy_gains_proj1_backend/src/main/java/com/revature/proj1/repos/UserRepository.java
@@ -5,10 +5,7 @@ import com.revature.proj1.users.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
--- fassy_gains_proj1_backend/src/main/java/com/revature/proj1/users/UserService.java
+++ fassy_gains_proj1_backend/src/main/java/com/revature/proj1/users/UserService.java
@@ -1,25 +1,24 @@
 package com.revature.proj1.users;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-import com.revature.proj1.factory.ScannerFactory;
-import com.revature.proj1.repos.UserRepository;
 import com.revature.proj1.users.User;
+import com.revature.proj1.repos.UserRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class UserService {
     private static User currentUser;
     private final Logger logger;
-    private final Scanner scanner;
     private final UserRepository userRepository;
     private final BCrypt.Hasher hasher;
+    private final String SALT = "xEZew5rGNU?tyoF.";
 
     public UserService() {
         this.logger = LogManager.getLogger(UserService.class);
-        this.scanner = ScannerFactory.getScanner();
         this.userRepository = new UserRepository();
         this.hasher = BCrypt.withDefaults();
         currentUser = null;
@@ -33,40 +32,10 @@ public class UserService {
     }
 
     public List<User> getAll() {
-       return userRepository.getAll();
+        return userRepository.getAll();
     }
 
-    public void register(){
-        System.out.println("\n-- User Registration --");
-        User user = buildUser(); // get the username and password
-
-        userRepository.create(user);
-    }
-
-    public User login(){
-        System.out.println("\n-- User Login --");
-        return validate();
-    }
-
-    public static User getCurrentUser(){
-        return currentUser;
-    }
-
-    private User validate(){
-
-        // user enters username, user enters password, build user object
-
-        User user = new User(getUsername(), getPassword());
-        if(isValid(user)){
-            return user;
-        } else{
-            return null;
-        }
-
-//        return isValid(user) ? user : null;
-    }
-
-    private boolean isValid(User user){
+    public User validate(User user){
         // user object passed in contains whatever the user entered when logging in
         // user entered information that can compare against the db info
 
@@ -74,78 +43,18 @@ public class UserService {
         // dbUser contains the user that's on the database
 
         if(dbUser != null){ // if it is null, the username doesn't exist
-            if(dbUser.getPassword().equals(user.getPassword())) {
+            if(dbUser.getPassword().equals(encryptPassword(user.getPassword()))) {
                 currentUser = dbUser;
-                return true;
+                return dbUser;
             }
         }
-
-        System.out.println("Invalid credentials.");
-        return false;
+        return null;
     }
 
-    private User buildUser(){
-        User user = new User(getUsername(), getPassword());
-        return !exists(user) ? user : buildUser();
-    }
-
-    private String getUsername(){
-        String username = "";
-        boolean valid = false;
-
-        while(!valid){
-            System.out.print("Username: ");
-            username = scanner.nextLine();
-
-            //TODO: Username validation
-            if(username.length() > 4){
-                valid = true;
-            } else {
-                System.out.println("Username must be longer than 4 characters.");
-            }
-        }
-        return username;
-    }
-
-    private String getPassword(){
-        String password = "";
-        boolean valid = false;
-
-        while(!valid){
-            System.out.print("Password: ");
-            password = scanner.nextLine();
-
-            //TODO: Password validation
-            if(password.length() > 4){
-                valid = true;
-            } else{
-                System.out.println("Password must be longer than 4 characters.");
-            }
-        }
-        return encryptPassword(password);
-    }
-
-    private boolean exists(User user){
-        // need to check if the username exists
-        if (userRepository.getByUsername(user.getUsername()) == null){
-            return false;
-        } else{
-            System.out.println("Username already exists.");
-            return true;
-        }
-
-        // if method returns null -> the user doesnt exist !(true)
-
-//        return !(userRepository.getUserByUsername(user.getUsername()) == null);
-
-        // if it does, let them know and return true
-
-        // if it doesn't return false
-    }
     private String encryptPassword(String password){
-        return hasher.hashToString(4, password.toCharArray());
+        return new String(
+                hasher.hash(4, SALT.getBytes(StandardCharsets.UTF_8),
+                        password.getBytes(StandardCharsets.UTF_8)),
+                StandardCharsets.UTF_8);
     }
-
-
-
 }
